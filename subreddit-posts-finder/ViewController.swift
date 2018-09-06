877f3b3099039151e3e95fdbf501a51fa79cbe89
jem.sai <jem.sai@whitecloak.com>
2018-09-06 13:42:06 +0800

    Initial Commit

---
+++ subreddit-posts-finder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  subreddit-posts-finder
+//
+//  Created by Jemimah Beryl M. Sai on 06/09/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
