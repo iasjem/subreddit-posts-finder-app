33068361c8901e913da774124cdf12821e61387a
jem.sai <jem.sai@whitecloak.com>
2018-09-07 14:07:54 +0800

    Modify a Swift file named SubscribeDisplay in Model folder to implement the Subscribe display model

--- subreddit-posts-finder/Model/Displays/SubscribeDisplay.swift
+++ subreddit-posts-finder/Model/Displays/SubscribeDisplay.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct SubscribeDisplay {
+    private(set) var displayName: String
+    private(set) var namePrefixed: String
+    private(set) var icon: URL?
+    private(set) var header: URL?
+    private(set) var subscribers: String
+    
+    init(displayName: String, namePrefixed: String, icon: URL?, header: URL?, subscribers: String) {
+        self.displayName = displayName
+        self.namePrefixed = namePrefixed
+        self.icon = icon
+        self.header = header
+        self.subscribers = subscribers
+    }
+}
