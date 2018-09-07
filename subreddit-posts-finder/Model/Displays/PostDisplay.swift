c0c968b6f930749a2764f1c94df5552a6cd1f0c4
jem.sai <jem.sai@whitecloak.com>
2018-09-07 14:07:14 +0800

    Modify a Swift file named PostDisplay in Model folder to implement the Post display model

--- subreddit-posts-finder/Model/Displays/PostDisplay.swift
+++ subreddit-posts-finder/Model/Displays/PostDisplay.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+struct PostDisplay {
+    private(set) var subreddit: String
+    private(set) var title: String
+    private(set) var selfText: String
+    private(set) var createdUTC: String
+    private(set) var imageSourceURL: URL?
+    private(set) var imageSourceWidth: Int?
+    private(set) var imageSourceHeight: Int?
+    
+    init(subreddit: String, title: String, selfText: String, createdUTC: String, imageSourceURL: URL?, imageSourceWidth: Int?, imageSourceHeight: Int?) {
+        self.subreddit = subreddit
+        self.title = title
+        self.selfText = selfText
+        self.createdUTC = createdUTC
+        self.imageSourceURL = imageSourceURL
+        self.imageSourceWidth = imageSourceWidth
+        self.imageSourceHeight = imageSourceHeight
+    }
+}
