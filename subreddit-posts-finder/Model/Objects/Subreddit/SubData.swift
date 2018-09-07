3a3dc05fa5f62fc863d8bfb301669128a2b764e0
jem.sai <jem.sai@whitecloak.com>
2018-09-07 13:49:01 +0800

    Add a Swift file named SubData in Model folder

---
+++ subreddit-posts-finder/Model/Objects/Subreddit/SubData.swift
@@ -0,0 +1,9 @@
+//
+//  SubData.swift
+//  subreddit-posts-finder
+//
+//  Created by Jemimah Beryl M. Sai on 07/09/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import Foundation
