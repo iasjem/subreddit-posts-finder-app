fc5f47ac4d7791ffbd0e6b1d9405a2292042059c
jem.sai <jem.sai@whitecloak.com>
2018-09-07 13:54:49 +0800

    Modify a Swift file named PostChildData in Model folder to implement the Post object model

--- subreddit-posts-finder/Model/Objects/Post/PostChildData.swift
+++ subreddit-posts-finder/Model/Objects/Post/PostChildData.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
 //
 
-import Foundation
+import ObjectMapper
+
+class PostChildData: Mappable {
+    var subreddit: String?
+    var title: String?
+    var selfText: String?
+    var createdUTC: Int?
+    var preview: PostPreview?
+    
+    required init?(map: Map) {  }
+    
+    func mapping(map: Map) {
+        subreddit <- map["subreddit_name_prefixed"]
+        title <- map["title"]
+        selfText <- map["selftext"]
+        createdUTC <- map["created_utc"]
+        preview <- map["preview"]
+    }
+}
