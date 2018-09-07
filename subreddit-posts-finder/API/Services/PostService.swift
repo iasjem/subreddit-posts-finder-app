ca965d1d2e5a046eed85a060066c34061f84cc6f
jem.sai <jem.sai@whitecloak.com>
2018-09-07 18:59:42 +0800

    Implement a function to set data for Post data services

--- subreddit-posts-finder/API/Services/PostService.swift
+++ subreddit-posts-finder/API/Services/PostService.swift
@@ -40,4 +40,17 @@ class PostService {
     
     // MARK: Setters
     
+    private func setData(from jsonData: [String: Any]) {
+        guard let post = Mapper<Post>().map(JSON: jsonData) else { return }
+        for x in 0..<10 {
+            guard let subreddit = post.data?.children![x].data?.subreddit else { return }
+            guard let title = post.data?.children![x].data?.title else { return }
+            guard let selfText = post.data?.children![x].data?.selfText else { return }
+            guard let createdUTC = post.data?.children![x].data?.createdUTC else { return }
+            guard let imageUrl = post.data?.children![x].data?.preview?.images![0].source?.url else { return }
+            guard let imageWidth = post.data?.children![x].data?.preview?.images![0].source?.width else { return }
+            guard let imageHeight = post.data?.children![x].data?.preview?.images![0].source?.height else { return }
+            postDisplay.append(PostDisplay(subreddit: subreddit, title: title, selfText: selfText, createdUTC: String(createdUTC), imageSourceURL: URL(string: imageUrl), imageSourceWidth: imageWidth, imageSourceHeight: imageHeight))
+        }
+    }
 }
