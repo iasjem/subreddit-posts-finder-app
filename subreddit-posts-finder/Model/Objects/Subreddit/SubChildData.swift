3fe8cab388ca1a432a561f1dd2c0963c8466bb8d
jem.sai <jem.sai@whitecloak.com>
2018-09-07 13:58:01 +0800

    Modify a Swift file named SubChildData in Model folder to implement the Subreddit object model

--- subreddit-posts-finder/Model/Objects/Subreddit/SubChildData.swift
+++ subreddit-posts-finder/Model/Objects/Subreddit/SubChildData.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
 //
 
-import Foundation
+import ObjectMapper
+
+class SubChildData: Mappable {
+    var displayName: String?
+    var namePrefixed: String?
+    var icon: String?
+    var header: String?
+    var subscribers: Int?
+    var url: String?
+    
+    required init?(map: Map) {  }
+    
+    func mapping(map: Map) {
+        displayName <- map["display_name"]
+        namePrefixed <- map["display_name_prefixed"]
+        icon <- map["icon_img"]
+        header <- map["header_img"]
+        subscribers <- map["subscribers"]
+        url <- map["url"]
+    }
+}
