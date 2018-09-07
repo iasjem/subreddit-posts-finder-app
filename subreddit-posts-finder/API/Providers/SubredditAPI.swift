a8fda1c9b4f148d5e498906ab5c2405c5bd29eed
jem.sai <jem.sai@whitecloak.com>
2018-09-07 15:16:22 +0800

    Modify a Swift file named SubredditAPI in Providers folder to implement API for Subreddit data

--- subreddit-posts-finder/API/Providers/SubredditAPI.swift
+++ subreddit-posts-finder/API/Providers/SubredditAPI.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
 //
 
-import Foundation
+import Moya
+
+// MARK: Enumeration
+
+enum SubredditAPI {
+    case getSubreddits
+    case getPosts(url: String)
+}
+
+// MARK: TargetType Protocol Implementation
+
+extension SubredditAPI: TargetType {
+    var baseURL: URL {  return URL(string: "https://api.reddit.com")!  }
+    
+    var path: String {
+        switch self {
+        case .getSubreddits:
+            return "/r/"
+        case .getPosts(let url):
+            return "\(url)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getSubreddits, .getPosts:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-type": "application/json"]
+    }
+    
+    var task: Task {
+        switch self {
+        case .getSubreddits, .getPosts:
+            return .requestPlain
+        }
+    }
+}
