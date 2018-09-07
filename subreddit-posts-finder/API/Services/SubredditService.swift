7e82e722131702fa745dfd456045b54c04715b4a
jem.sai <jem.sai@whitecloak.com>
2018-09-07 19:02:13 +0800

    Implement a function to set data for Subreddit data services

--- subreddit-posts-finder/API/Services/SubredditService.swift
+++ subreddit-posts-finder/API/Services/SubredditService.swift
@@ -41,4 +41,17 @@ class SubredditService {
     
     // MARK: Setters
     
+    private func setData(from jsonData: [String: Any]) {
+        guard let subreddit = Mapper<Subreddit>().map(JSON: jsonData) else { return }
+        for x in 0..<8 {
+            guard let displayName = subreddit.data?.children![x].data?.displayName else { return }
+            guard let namePrefixed = subreddit.data?.children![x].data?.namePrefixed else { return }
+            guard let icon = subreddit.data?.children![x].data?.icon else { return }
+            guard let header = subreddit.data?.children![x].data?.header else { return }
+            guard let subscribers = subreddit.data?.children![x].data?.subscribers else { return }
+            guard let url = subreddit.data?.children![x].data?.url else { return }
+            searchDisplay.append(SearchDisplay(displayName: displayName, namePrefixed: namePrefixed, icon: URL(string: icon), url: URL(string: url)!))
+            subscribeDisplay.append(SubscribeDisplay(displayName: displayName, namePrefixed: namePrefixed, icon: URL(string: icon), header: URL(string: header), subscribers: String(subscribers)))
+        }
+    }
 }
