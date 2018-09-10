7cb42059e992e841914f55385067056bc9129e9e
jem.sai <jem.sai@whitecloak.com>
2018-09-10 14:03:29 +0800

    Add Swift and XIB files named Subscribe Cell in View folder

---
+++ subreddit-posts-finder/View/SubscribeCell/Controller/SubscribeCell.swift
@@ -0,0 +1,18 @@
+//
+//  SubscribeCell.swift
+//  subreddit-posts-finder
+//
+//  Created by Jemimah Beryl M. Sai on 10/09/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class SubscribeCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
