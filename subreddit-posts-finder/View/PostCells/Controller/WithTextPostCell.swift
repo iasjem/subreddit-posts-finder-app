8782137426c2e50f78f47382370c7be070e70633
jem.sai <jem.sai@whitecloak.com>
2018-09-10 13:14:19 +0800

    Add Swift and XIB files named With Text Post Cell in View folder

---
+++ subreddit-posts-finder/View/PostCells/Controller/WithTextPostCell.swift
@@ -0,0 +1,18 @@
+//
+//  WithTextPostCell.swift
+//  subreddit-posts-finder
+//
+//  Created by Jemimah Beryl M. Sai on 10/09/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class WithTextPostCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
