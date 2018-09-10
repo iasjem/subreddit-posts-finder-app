a7d5177bff31ddeadd813a277b9ff8e0069a4a4a
jem.sai <jem.sai@whitecloak.com>
2018-09-10 13:15:00 +0800

    Add Swift and XIB files named With Wide Image Post Cell in View folder

---
+++ subreddit-posts-finder/View/PostCells/Controller/WithWideImagePostCell.swift
@@ -0,0 +1,18 @@
+//
+//  WithImageCell.swift
+//  subreddit-posts-finder
+//
+//  Created by Jemimah Beryl M. Sai on 10/09/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class WithWideImagePostCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
