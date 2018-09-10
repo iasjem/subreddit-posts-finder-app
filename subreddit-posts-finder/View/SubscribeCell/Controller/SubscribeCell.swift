2a03dab984a10539df5633c3402a9506809eef6c
jem.sai <jem.sai@whitecloak.com>
2018-09-10 17:55:17 +0800

    Modify Swift file named SubscribeCell in View folder to connect outlets from its XIB file

--- subreddit-posts-finder/View/SubscribeCell/Controller/SubscribeCell.swift
+++ subreddit-posts-finder/View/SubscribeCell/Controller/SubscribeCell.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class SubscribeCell: UICollectionViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
+    @IBOutlet weak var blueCellDisplayNameLabel: UILabel!
+    @IBOutlet weak var blueCellNamePrefixedLabel: UILabel!
+    @IBOutlet weak var blueCellSubscribersLabel: UILabel!
+    @IBOutlet weak var blueCellIconImageView: UIImageView!
+    @IBOutlet weak var yellowCellDisplayNameLabel: UILabel!
+    @IBOutlet weak var yellowCellNamePrefixedLabel: UILabel!
+    @IBOutlet weak var yellowCellSubscribersLabel: UILabel!
+    @IBOutlet weak var yellowCellIconImageView: UIImageView!
+    @IBOutlet weak var whiteCellDisplayNameLabel: UILabel!
+    @IBOutlet weak var whiteCellNamePrefixedLabel: UILabel!
+    @IBOutlet weak var whiteCellSubscribersLabel: UILabel!
+    @IBOutlet weak var whiteCellIconImageView: UIImageView!
+    @IBOutlet weak var whiteCellHeaderImageView: UIImageView!
 }
