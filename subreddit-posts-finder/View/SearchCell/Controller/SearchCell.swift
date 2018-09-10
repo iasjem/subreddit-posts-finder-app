727d6aefdd17365ffd6bd338f28272677b826896
jem.sai <jem.sai@whitecloak.com>
2018-09-10 13:11:55 +0800

    Add Swift and XIB files named Search Cell in View folder

---
+++ subreddit-posts-finder/View/SearchCell/Controller/SearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchCell.swift
+//  subreddit-posts-finder
+//
+//  Created by Jemimah Beryl M. Sai on 10/09/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
