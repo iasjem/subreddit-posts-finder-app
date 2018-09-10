3d1f4899265850f63deeb984e609e7e57da57d3a
jem.sai <jem.sai@whitecloak.com>
2018-09-10 17:51:47 +0800

    Add a Swift file named RoundImageView in View folder

---
+++ subreddit-posts-finder/View/Designables/RoundImageView.swift
@@ -0,0 +1,9 @@
+//
+//  RoundImageView.swift
+//  subreddit-posts-finder
+//
+//  Created by Jemimah Beryl M. Sai on 10/09/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import Foundation
