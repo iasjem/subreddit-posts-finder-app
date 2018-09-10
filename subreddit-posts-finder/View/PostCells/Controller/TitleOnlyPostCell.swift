5655f43bc692c1118fac666b801b0c560c76ab1d
jem.sai <jem.sai@whitecloak.com>
2018-09-10 13:12:51 +0800

    Add Swift and XIB files named Title Only Post Cell in View folder

---
+++ subreddit-posts-finder/View/PostCells/Controller/TitleOnlyPostCell.swift
@@ -0,0 +1,18 @@
+//
+//  TitleOnlyPostCell.swift
+//  subreddit-posts-finder
+//
+//  Created by Jemimah Beryl M. Sai on 10/09/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class TitleOnlyPostCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
